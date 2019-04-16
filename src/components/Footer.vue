5e5d8efc51e6f40c45be7c7879947f282e205e43
Jan Zapał <janek.zapal@gmail.com>
2019-04-16 21:12:59 +0200

    version

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="app-footer text-left text-muted font-sm">
     <span></span>
     <span class="ml-auto">Projekt i wykonanie GeekMan/Misal System</span>&nbsp;
-    <span>v.18.03.2019</span>
+    <span>v.16.04.2019</span>
   </footer>
 </template>
 <script>
