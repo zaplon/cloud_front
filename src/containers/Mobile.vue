09d4fb4e095e50e621e380bc763f71152f57c559
Jan Zapał <janek.zapal@gmail.com>
2019-07-20 19:01:32 +0200

    zdjęcia

--- src/containers/Mobile.vue
+++ src/containers/Mobile.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="bg-light mt-4">
-        <div class="container">
-            <router-view></router-view>
+    <div class="align-items-center">
+        <header class="app-header navbar setup-header">
+            <b-link class="navbar-brand" to="#"></b-link>
+            <button class="btn-link mr-2" @click="logOut">Wyloguj</button>
+        </header>
+        <div class="app-body">
+            <div class="container">
+                <router-view></router-view>
+            </div>
         </div>
     </div>
 </template>
--- src/containers/Setup.vue
+++ src/containers/Setup.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="align-items-center">
         <header class="app-header navbar setup-header">
+            <b-link class="navbar-brand" to="#"></b-link>
             <b-dropdown id="ddown-divider" variant="link" class="m-2 pull-right">
                 <template slot="button-content"><i class="fa fa-user"></i></template>
                 <b-dropdown-divider></b-dropdown-divider>
                 <b-dropdown-item-button @click="logOut">Wyloguj</b-dropdown-item-button>
             </b-dropdown>
-            <b-link class="navbar-brand" to="#"></b-link>
         </header>
         <div class="app-body">
             <div class="container">
